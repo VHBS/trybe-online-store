dab8660a4b6c83230359f679e0ff40cfc16d21f3
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-11-19 18:34:51 -0300

    feat: requisito 09

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,6 +4,21 @@ import { Link } from 'react-router-dom';
 import carrinho from '../images/carrinho.png';
 
 class ProductDetails extends Component {
+  addToCart = async () => {
+    const { location:
+      { state: { thumbnail, title, price, id, attributes } } } = this.props;
+
+    const objProducts = { thumbnail, title, price, id, attributes };
+    const itemsLocalStorage = JSON.parse(localStorage.getItem('cartItems'));
+    const emptyLocalStorage = [];
+
+    const test = (itemsLocalStorage === null) ? emptyLocalStorage
+      : itemsLocalStorage;
+
+    localStorage.setItem('cartItems',
+      JSON.stringify([...test, objProducts]));
+  }
+
   render() {
     const { location: { state: { thumbnail, title, price } } } = this.props;
 
@@ -18,6 +33,17 @@ class ProductDetails extends Component {
           <h4 data-testid="product-detail-name">{title}</h4>
           <p>{ price }</p>
           <img src={ thumbnail } alt={ title } />
+          <Link
+            to={ { pathname: 'Cart' } }
+          >
+            <button
+              type="button"
+              onClick={ () => this.addToCart() }
+              data-testid="product-detail-add-to-cart"
+            >
+              Adicionar ao Carrinho
+            </button>
+          </Link>
         </div>
       </>
     );
