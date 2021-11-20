6a65df1785eb312f7942eceb3e419ac1320460d1
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-11-20 20:09:24 -0300

    feat: requisito 11

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import ReactStars from 'react-rating-stars-component';
 import carrinho from '../images/carrinho.png';
 
 class ProductDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      description: '',
+      stars: 0,
+      savedRatings: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getLocalstrorageRatings();
+  }
+
   addToCart = async () => {
     const { location:
       { state: { thumbnail, title, price, id, attributes } } } = this.props;
@@ -19,8 +35,37 @@ class ProductDetails extends Component {
       JSON.stringify([...test, objProducts]));
   }
 
+  ratingChanged = (newRating) => {
+    // console.log(newRating);
+    this.setState({ stars: newRating });
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+    console.log('value: ', value, 'name: ', name);
+  }
+
+  saveRating = () => {
+    const { email, description, stars } = this.state;
+    const objRating = { email, description, stars };
+    const localStorageSaved = JSON.parse(localStorage.getItem('rating'));
+    const items = localStorageSaved === null ? [] : localStorageSaved;
+
+    localStorage.setItem('rating', JSON.stringify([...items, objRating]));
+    this.setState({ email: '', description: '', stars: 0 });
+    // this.getLocalstrorageRatings();
+  }
+
+  getLocalstrorageRatings = () => {
+    const savedRatings = JSON.parse(localStorage.getItem('rating'));
+    // console.log('get', typeof savedRatings[0]);
+
+    this.setState({ savedRatings });
+  }
+
   render() {
     const { location: { state: { thumbnail, title, price } } } = this.props;
+    const { email, description, stars, savedRatings } = this.state;
 
     return (
       <>
@@ -45,6 +90,56 @@ class ProductDetails extends Component {
             </button>
           </Link>
         </div>
+        <form>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+
+          <ReactStars
+            count={ 5 }
+            onChange={ this.ratingChanged }
+            size={ 24 }
+            activeColor="#ffd700"
+            value={ stars }
+          />
+
+          <textarea
+            name="description"
+            placeholder="Mensagem (Opcional)"
+            value={ description }
+            data-testid="product-detail-evaluation"
+            onChange={ this.handleChange }
+          />
+
+          <button
+            type="button"
+            onClick={ this.saveRating }
+            disabled={ email.length === 0 || stars === 0 }
+          >
+            Enviar
+          </button>
+        </form>
+
+        {savedRatings &&
+          savedRatings.map((rating, index) => (
+            <div key={ `${rating.email}${index}` }>
+              {console.log(typeof rating)}
+              <p>{ rating.email }</p>
+              <ReactStars
+                count={ 5 }
+                // onChange={ this.ratingChanged }
+                size={ 24 }
+                activeColor="#ffd700"
+                value={ rating.stars }
+              />
+              <p>{ rating.description }</p>
+            </div>
+          ))
+        }
       </>
     );
   }
